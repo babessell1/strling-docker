3aff802f7f1931bd82725276365dd0aaae24fa26
babessell1 <babessell@gmail.com>
2023-05-18 19:14:12 -0400

    s

--- run_strling.sh
+++ run_strling.sh
@@ -2,13 +2,17 @@
 
 cram=$1
 fasta=$2
-md5=$3
+md5file=$3
 
-MD_OUT=($(md5sum $cram))
-if [[ $MD_OUT != $md5]]; then
+md5sum=$(echo $md5file)
+
+MD_OUT=($($MD5SUM $cram))
+if [[ $MD_OUT != $md5]]
+then
     echo "MD5 sum does not match expected value!" 1>&2
     exit 1
 fi
+echo $MD_OUT
 
 sname=$(basename "$cram" .cram)
 
