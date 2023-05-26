83efcd8898449106078c027df58fa2e66b28a843
babessell1 <babessell@gmail.com>
2023-05-26 15:32:50 -0400

    [fix] pass crai array to parallel function

--- run_strling.sh
+++ run_strling.sh
@@ -5,19 +5,21 @@ fasta=$2
 cramsidx=$3
 fastaidx=$4
 
+# faidx to same folder as fasta
+fname=$(basename "$fasta")
+directory=$(dirname "$fasta")
+cp "$fastaidx" "${directory}/${fname}.fai"
+
 process_cram_file() {
     cram_file=$1
+    cramidx_file=$2
 
     chmod 777 "$cram_file"
 
     # copy crais to same folder as crams (tibanna puts in own folder)
     sname=$(basename "$cram_file" .cram)
     directory=$(dirname "$cram_file")
-    cp $cramsidx "${directory}/${sname}.cram.crai"
-
-    fname=$(basename "$fasta")
-    directory=$(dirname "$fasta")
-    cp "$fastaidx" "${directory}/${fname}.fai"
+    cp "$cramidx_file" "${directory}/${sname}.cram.crai"
 
     mkdir -p str-bins/
     /usr/local/bin/strling extract -f "$fasta" "$cram_file" "str-bins/${sname}.bin"
@@ -31,7 +33,7 @@ process_cram_file() {
 export -f process_cram_file
 
 # Run the process_cram_file function in parallel for each CRAM file
-parallel --jobs 2 process_cram_file ::: $crams
+parallel --jobs 2 process_cram_file ::: $crams $cramsidx
 
 # Ensure all parallel jobs are completed before proceeding
 wait
