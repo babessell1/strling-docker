10f6b263f6f2574474572ee9e4c7cea21c9bddb1
babessell1 <babessell@gmail.com>
2023-05-26 14:57:57 -0400

    [fix] copy index to cram folders

--- run_strling.sh
+++ run_strling.sh
@@ -10,7 +10,14 @@ process_cram_file() {
 
     chmod 777 "$cram_file"
 
+    # copy crais to same folder as crams (tibanna puts in own folder)
     sname=$(basename "$cram_file" .cram)
+    directory=$(dirname "$cram_file")
+    cp $cramsidx "${directory}/${sname}.cram.crai"
+
+    fname=$(basename "$fasta")
+    directory=$(dirname "$fasta")
+    cp "$fastaidx" "${directory}/${fname}.fai"
 
     mkdir -p str-bins/
     /usr/local/bin/strling extract -f "$fasta" "$cram_file" "str-bins/${sname}.bin"
