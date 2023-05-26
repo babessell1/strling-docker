e768bdf2e4c2e2aef891050056fd050303f11693
babessell1 <babessell@gmail.com>
2023-05-26 16:25:26 -0400

    [change] let cwl handle parallel?

--- run_strling.sh
+++ run_strling.sh
@@ -6,6 +6,10 @@ cramsidx=$3
 fastaidx=$4
 
 echo "$crams"
+chmod 777 "$crams"
+chmod 777 "$fasta"
+chmod 777 "$cramsidx"
+chmod 777 "$fastaidx"
 
 # faidx to same folder as fasta
 fname=$(basename "$fasta")
@@ -15,32 +19,18 @@ echo "$fname"
 echo "$fastaidx"
 cp "$fastaidx" "${directory}/${fname}.fai"
 
-process_cram_file() {
-    cram_file=$1
-    cramidx_file=$2
 
-    #chmod 777 "$cram_file"
+# copy crais to same folder as crams (tibanna puts in own folder)
+sname=$(basename "$crams" .cram)
+directory=$(dirname "$crams")
+cp "$cramidx_file" "${directory}/${sname}.cram.crai"
 
-    # copy crais to same folder as crams (tibanna puts in own folder)
-    sname=$(basename "$cram_file" .cram)
-    directory=$(dirname "$cram_file")
-    cp "$cramidx_file" "${directory}/${sname}.cram.crai"
+mkdir -p str-bins/
+/usr/local/bin/strling extract -f "$fasta" "$crams" "str-bins/${sname}.bin"
+mkdir -p str-results/
+mkdir -p "str-results/${sname}/"
+mkdir -p "str-logs/${sname}/"
 
-    mkdir -p str-bins/
-    /usr/local/bin/strling extract -f "$fasta" "$cram_file" "str-bins/${sname}.bin"
-    mkdir -p str-results/
-    mkdir -p "str-results/${sname}/"
-    mkdir -p "str-logs/${sname}/"
-
-    /usr/local/bin/strling call --output-prefix "str-results/${sname}/${sname}" -f "$fasta" "$cram_file" "str-bins/${sname}.bin" > "str-logs/${sname}.log"
-}
-
-export -f process_cram_file
-
-# Run the process_cram_file function in parallel for each CRAM file
-parallel --jobs 2 process_cram_file ::: $crams $cramsidx
-
-# Ensure all parallel jobs are completed before proceeding
-wait
+/usr/local/bin/strling call --output-prefix "str-results/${sname}/${sname}" -f "$fasta" "$crams" "str-bins/${sname}.bin" > "str-logs/${sname}.log"
 
 
