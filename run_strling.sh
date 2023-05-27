d9d7ecbed305364bd79000a6d28682eff3d50f00
babessell1 <babessell@gmail.com>
2023-05-27 13:19:06 -0400

    [change] data1 folder instead of ro

--- run_strling.sh
+++ run_strling.sh
@@ -10,31 +10,36 @@ echo "$fasta"
 echo "$cramsidx"
 echo "$fastaidx"
 
-samtools faidx $fasta
+ref_dir="/data1/input/references/"
+cram_dir="/data1/input/crams/"
+
 
 # faidx to same folder as fasta
 fname=$(basename "$fasta")
-directory=$(dirname "$fasta")
+ro_ref_dir=$(dirname "$fasta")
 #ln -s "$fastaidx" "${directory}/${fname}.fai"
+
+samtools faidx "${ref_dir}/${fname}"
 
 process_cram_file() {
     cram_file=$1
     echo "$cram_file"
-    #cramidx_file=$2
-    samtools index $cram_file
 
     # copy crais to same folder as crams (tibanna puts in own folder)
     sname=$(basename "$cram_file" .cram)
-    directory=$(dirname "$cram_file")
+    ro_cram_dir=$(dirname "$cram_file")
     #ln -s "$cramsidx_file" "${directory}/${sname}.cram.crai"
 
+    #cramidx_file=$2
+    samtools index "${cram_dir}/${sname}.cram"
+
     mkdir -p str-bins/
-    /usr/local/bin/strling extract -f "$fasta" "$cram_file" "str-bins/${sname}.bin"
+    /usr/local/bin/strling extract -f "${ref_dir}/${fname}" "${cram_dir}/${sname}.cram" "str-bins/${sname}.bin"
     mkdir -p str-results/
     mkdir -p "str-results/${sname}/"
     mkdir -p "str-logs/${sname}/"
 
-    /usr/local/bin/strling call --output-prefix "str-results/${sname}/${sname}" -f "$fasta" "$cram_file" "str-bins/${sname}.bin" > "str-logs/${sname}.log"
+    /usr/local/bin/strling call --output-prefix "str-results/${sname}/${sname}" -f "${ref_dir}/${fname}" "${cram_dir}/${sname}.cram" "str-bins/${sname}.bin" > "str-logs/${cname}.log"
 }
 
 export -f process_cram_file
