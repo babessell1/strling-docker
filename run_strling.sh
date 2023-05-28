cfb06a3c79aba5053e1544fd735b7e0166367dbe
babessell1 <babessell@gmail.com>
2023-05-28 14:42:30 -0400

    l

--- run_strling.sh
+++ run_strling.sh
@@ -17,19 +17,20 @@ ro_ref_dir=$(dirname "$fasta")
 sname=$(basename "$cram" .cram)
 ro_cram_dir=$(dirname "$cram")
 
-ls /mnt/data1/input | echo
-echo "$(ls)"
-echo "$(pwd)"
-echo "sname"
-echo "$sname"
-echo "$fname"
+#ls /mnt/data1/input | echo
+echo "ls: $(ls)"
+echo "pwd: $(pwd)"
+echo "sname: $sname"
+echo "fname: $fname"
+echo " sub ls: $(ls */)"
+echo "sub pwd: $(pwd */)"
 
 #ln -s "$fastaidx" "${ro_ref_dir}/${fname}.fai"
 
 mv "/data1/input/cramsidx/${sname}.cram.crai" "data1/input/crams/${sname}.cram.crai"
 
 cram="/data1/input/crams/${sname}.cram"
-fasta="/data1/input/references/${fname}
+fasta="/data1/input/references/${fname}"
 
 mkdir -p str-bins/
 /usr/local/bin/strling extract -f "$fasta" "$cram" "str-bins/${sname}.bin"
