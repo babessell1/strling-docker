8f03ceca09f2721434cdcf04a05a08e528125f56
babessell1 <babessell@gmail.com>
2023-05-27 20:08:07 -0400

    ahhh

--- run_strling.sh
+++ run_strling.sh
@@ -5,21 +5,11 @@ fasta=$2
 cramidx=$3
 fastaidx=$4
 
-fname=$(basename "$fasta")
-ro_ref_dir=$(dirname "$fasta")
-ln -s "$fastaidx" "${directory}/${fname}.fai"
-
-samtools faidx "${ref_dir}/${fname}"
-
-
-mkdir -p str-bins/
-/usr/local/bin/strling extract -f "$fa" "${cram_dir}/${sname}.cram" "str-bins/${sname}.bin"
-mkdir -p str-results/
-mkdir -p "str-results/${sname}/"
-mkdir -p "str-logs/${sname}/"
-
-/usr/local/bin/strling call --output-prefix "str-results/${sname}/${sname}" -f "$fa" "${cram_dir}/${sname}.cram" "str-bins/${sname}.bin" > "str-logs/${cname}.log"
-
+echo "Start"
+echo "$cram"
+echo "$fasta"
+echo "$cramidx"
+echo "$fastaidx"
 
 fname=$(basename "$fasta")
 ro_ref_dir=$(dirname "$fasta")
@@ -30,10 +20,11 @@ ro_cram_dir=$(dirname "$cram_file")
 ln -s "$fastaidx" "${ro_ref_dir}/${fname}.fai"
 ln -s "$cramidx" "${ro_cram_dir}/${sname}.cram.crai"
 
+
 mkdir -p str-bins/
 /usr/local/bin/strling extract -f "$fasta" "$cram" "str-bins/${sname}.bin"
 mkdir -p str-results/
 mkdir -p "str-results/${sname}/"
 mkdir -p "str-logs/${sname}/"
-/usr/local/bin/strling call --output-prefix "str-results/${sname}/${sname}" -f "$fasta" "$cram" "str-bins/${sname}.bin" > "str-logs/${sname}.log"
 
+/usr/local/bin/strling call --output-prefix "str-results/${sname}/${sname}" -f "$fasta" "$cram" "str-bins/${sname}.bin" > "str-logs/${sname}.log"
