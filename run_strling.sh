b59a097b2b559148feaee3440adbe4e7be914458
babessell1 <babessell@gmail.com>
2023-05-28 18:51:01 -0400

    fixed, put parallel back in

--- run_strling.sh
+++ run_strling.sh
@@ -1,42 +1,39 @@
 #!/bin/bash
 
-cram=$1
-fasta=$3
-cramidx=$4
-fastaidx=$6
+call_strling() {
+    cram="$1"
+    fasta="$2"
+    cramidx="$3"
 
-echo "Start"
-echo "$cram"
-echo "$fasta"
-echo "$cramidx"
-echo "$fastaidx"
+    fname=$(basename "$fasta")
+    ro_ref_dir=$(dirname "$fasta")
 
-fname=$(basename "$fasta")
-ro_ref_dir=$(dirname "$fasta")
+    sname=$(basename "$cram" .cram)
+    ro_cram_dir=$(dirname "$cram")
+    ro_cramidx_dir=$(dirname "$cramidx")
 
-sname=$(basename "$cram" .cram)
-ro_cram_dir=$(dirname "$cram")
-ro_cramidx_dir=$(dirname "$cramidx")
+    cp "${ro_cramidx_dir}/${sname}.cram.crai" "${ro_cram_dir}/${sname}.cram.crai"
 
-#ls /mnt/data1/input | echo
-echo "ls: $(ls)"
-echo "pwd: $(pwd)"
-echo "sname: $sname"
-echo "fname: $fname"
-echo "sub ls: $(ls /mnt/*)"
-echo "sub pwd: $(pwd /mnt/*)"
+    mkdir -p str-bins/
+    /usr/local/bin/strling extract -f "$fasta" "$cram" "str-bins/${sname}.bin"
+    mkdir -p str-results/
+    mkdir -p "str-results/${sname}/"
+    mkdir -p "str-logs/${sname}/"
 
-#ln -s "$fastaidx" "${ro_ref_dir}/${fname}.fai"
+    /usr/local/bin/strling call --output-prefix "str-results/${sname}/${sname}" -f "$fasta" "$cram" "str-bins/${sname}.bin"
+    echo "done"
+}
 
-cp "${ro_cramidx_dir}/${sname}.cram.crai" "${ro_cram_dir}/${sname}.cram.crai"
+# Assign the command-line arguments to variables
+cram1="$1"
+cram2="$2"
+fasta="$3"
+cramidx1="$4"
+cramidx2="$5"
+fastaidx="$6"
 
-#cram="/data1/input/crams/${sname}.cram"
-#fasta="/data1/input/references/${fname}"
+# Define the function to be executed in parallel
+export -f call_strling
 
-mkdir -p str-bins/
-/usr/local/bin/strling extract -f "$fasta" "$cram" "str-bins/${sname}.bin"
-mkdir -p str-results/
-mkdir -p "str-results/${sname}/"
-mkdir -p "str-logs/${sname}/"
-
-/usr/local/bin/strling call --output-prefix "str-results/${sname}/${sname}" -f "$fasta" "$cram" "str-bins/${sname}.bin" > "str-logs/${sname}.log"
+# Run the script in parallel for both cram1 and cram2
+parallel call_strling ::: "$cram1" "$cram2" ::: "$fasta" "$fasta" ::: "$cramidx1" "$cramidx2"
