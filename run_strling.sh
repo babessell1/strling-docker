bd63d146d84d0286fee20dd7e17ef13373f90c9e
babessell1 <babessell@gmail.com>
2023-07-07 14:10:42 -0400

    [fix] threads arg

--- run_strling.sh
+++ run_strling.sh
@@ -29,7 +29,7 @@ process_file() {
     
     # strling requires idx in same folder
     #cp "${ro_cramidx_dir}/${bname}.cram.crai" "${ro_cram_dir}/${bname}.cram.crai"
-    samtools index --threads 2 "$cram"
+    samtools index -@ 2 "$cram"
 
     # extract repetitive region binaries
     /usr/local/bin/strling extract -f "$fasta" "$cram" "output/${bname}.bin"
